4d319cd3343f28eb942a118a04191b130526c1a7
NahianNahin <nahian1927040@stud.kuet.ac.bd>
2022-11-25 23:42:51 +0600

    working with stripe

--- src/pages/Payment/CheckoutForm.js
+++ src/pages/Payment/CheckoutForm.js
@@ -1,10 +1,90 @@
-import React from 'react';
+import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import React, { useEffect, useState } from 'react';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({ item }) => {
+    const { price, username, email, _id } = item;
+    const [cardError, setCardError] = useState('');
+    const [cardSuccess, setCardSuccess] = useState('');
+    const [cardTransation, setCardTransation] = useState('');
+    const [cardLoading, setCardLoading] = useState(false);
+    const stripe = useStripe();
+    const elements = useElements();
+    const [clientSecret, setClientSecret] = useState("");
+
+
+    useEffect(() => {
+        // Create PaymentIntent as soon as the page loads
+        fetch("http://localhost:5000/create-payment-intent", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ price }),
+        })
+            .then((res) => res.json())
+            .then((data) => setClientSecret(data.clientSecret));
+    }, [price]);
+
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        if (!stripe || !elements) {
+            return;
+        }
+
+        const card = elements.getElement(CardElement);
+
+        if (card == null) {
+            return;
+        }
+
+        const { error } = await stripe.createPaymentMethod({
+            type: 'card',
+            card,
+        });
+        if (error) {
+            console.log(error.message);
+            setCardError(error.message);
+        }
+        else {
+            setCardError('')
+        }
+        setCardSuccess('');
+        setCardLoading(true);
+    }
     return (
-        <div>
-            
-        </div>
+        <>
+            <form onSubmit={handleSubmit}>
+                <CardElement
+                    options={{
+                        style: {
+                            base: {
+                                fontSize: '16px',
+                                color: '#424770',
+                                '::placeholder': {
+                                    color: '#aab7c4',
+                                },
+                            },
+                            invalid: {
+                                color: '#9e2146',
+                            },
+                        },
+                    }}
+                />
+                <button
+                    className='btn mt-5 btn-sm bg-gradient-to-r from-primary to-secondary border-0 text-base-100 rounded-0'
+                    type="submit"
+                    disabled={!stripe}>
+                    Pay
+                </button>
+            </form>
+            <p className='text-error p-3'>{cardError}</p>
+            <p className='text-primary p-3'>{cardSuccess}</p>
+            {
+                cardSuccess && <p>Transation Id : {cardTransation}</p>
+            }
+        </>
     );
 };
 
--- src/pages/Payment/Payment.js
+++ src/pages/Payment/Payment.js
@@ -1,7 +1,11 @@
+import { Elements } from '@stripe/react-stripe-js';
+import { loadStripe } from '@stripe/stripe-js';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
-
+import CheckoutForm from './CheckoutForm';
+const stripePromise = loadStripe(process.env.REACT_APP_PK_TEST_KEY);
 const Payment = () => {
+    
     const item = useLoaderData();
     console.log(item);
     return (
@@ -9,6 +13,13 @@ const Payment = () => {
             <p className='text-3xl mb-5'>Payment</p>
             <p>Hello <span className='font-bold'>{item.username}</span></p>
             <p>Please, pay <span className='font-bold'>{item.price} TK</span> for your Selected Items <span className='font-bold'>{item.porduct_name}</span></p>
+            <div className='w-96 m-5 p-10 shadow-md bg-base-100 rounded-md'>
+                <Elements stripe={stripePromise}>
+                    <CheckoutForm
+                        item={item}
+                    />
+                </Elements>
+            </div>
         </div>
     );
 };
